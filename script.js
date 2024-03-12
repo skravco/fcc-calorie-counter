e2ce709b3399f172f7eb4fce199b55cabcfd0f9d
skravco <skravco92@gmail.com>
2024-03-12 17:35:10 +0100

    replace loop with regex | incorp template literal

--- script.js
+++ script.js
@@ -7,12 +7,27 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-  const strArray = str.split('');
-  const cleanStrArray = [];
-
-  for (let i = 0; i < strArray.length; i++) {
-    if (!["+", "-", " "].includes(strArray[i])) {
-      cleanStrArray.push(strArray[i])
-    }
-  }
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
 }
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+function addEntry() {
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+  targetInputContainer.innerHTML += HTMLString;
+}
+addEntryButton.addEventListener("click", addEntry);
